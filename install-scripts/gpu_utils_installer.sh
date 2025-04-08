fcb4b7ed88334dfd485fd018a6b6a69e76a7e209
bondtheroot <b0n5j4m3s@gmail.com>
2025-04-08 14:57:15 +0530

    Added GPU Utils installer script

--- install-scripts/gpu_utils_installer.sh
+++ install-scripts/gpu_utils_installer.sh
@@ -1,4 +1,6 @@
 echo "Installing nvtop, nvidia-smi and missioncenter"
 apt install nvtop nvidia-utils-550 flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo "Enter Y when prompted to continue installation of MissionCenter:"
 flatpak install flathub io.missioncenter.MissionCenter
 echo "To run missioncenter, run flatpak run io.missioncenter.MissionCenter"
